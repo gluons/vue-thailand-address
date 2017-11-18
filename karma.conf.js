9d9319d7fa936e86c09871263145a7896c052786
Saran Tanpituckpong <sarunta@gmail.com>
2017-11-18 19:25:25 +0700

    :wrench: Always use single run on `test` script

--- karma.conf.js
+++ karma.conf.js
@@ -84,7 +84,7 @@ module.exports = function (config) {
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: process.env.CI ? true : false,
+		singleRun: false,
 
 		// Concurrency level
 		// how many browser should be started simultaneous
