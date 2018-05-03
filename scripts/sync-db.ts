320dee8779306f31133c72cd173d41dacf6ed293
Saran Tanpituckpong <sarunta@gmail.com>
2018-05-03 23:24:06 +0700

    :recycle: Use partial import instead

--- scripts/sync-db.ts
+++ scripts/sync-db.ts
@@ -1,7 +1,5 @@
-import * as path from 'path';
+import { resolve } from 'path';
 import copyOverwrite from './lib/copy-overwrite';
-
-const { resolve } = path;
 
 const srcPath: string = resolve(__dirname, '../dependencies/jquery.Thailand.js/jquery.Thailand.js/database/db.json');
 const destPath: string = resolve(__dirname, '../src/data/db.json');
