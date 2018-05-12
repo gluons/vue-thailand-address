c9eef05f0af93d71f5406cfe564f4167c2e66843
Saran Tanpituckpong <sarunta@gmail.com>
2018-05-12 17:17:56 +0700

    :building_construction: Change test utils from `avoriaz` to `vue-test-utils`
    `avoriaz` is going to be deprecated.

--- test/TypeaheadInput.spec.ts
+++ test/TypeaheadInput.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-expression */
-import { mount } from 'avoriaz';
+import { mount } from '@vue/test-utils';
 import { expect } from 'chai';
 
 import TypeaheadInput from '@/components/TypeaheadInput.vue';
@@ -19,18 +19,19 @@ describe('TypeaheadInput', () => {
 	});
 
 	describe('Data', () => {
-		let data: any = wrapper.data();
+		let vm: any = wrapper.vm;
 
 		it(`should has 'selectedIndex' with expected data`, () => {
-			expect(data.selectedIndex).to.equal(5);
+			expect(vm.selectedIndex).to.equal(5);
 		});
 		it(`should has 'autocompleteCount' with expected data`, () => {
-			expect(data.autocompleteCount).to.equal(10);
+			expect(vm.autocompleteCount).to.equal(10);
 		});
 	});
 	describe('Props', () => {
 		it(`should has 'target' prop with expected value`, () => {
-			expect(wrapper.getProp('target')).to.equal('province');
+			expect(wrapper.props().target).to.exist;
+			expect(wrapper.props().target).to.equal('province');
 		});
 	});
 	describe('Methods', () => {
