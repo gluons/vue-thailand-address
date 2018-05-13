0a4b59943337535ad4d3a0ebb11c7a6e0c9743c9
Saran Tanpituckpong <sarunta@gmail.com>
2018-05-13 13:39:00 +0700

    :sparkles: Export `AddressForm` component

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Vue, { VueConstructor } from 'vue';
 
 import AddressForm from './components/AddressForm.vue';
 
-/* tslint:disable: no-namespace */
 declare global {
 	interface Window {
 		Vue: typeof Vue;
@@ -22,6 +21,11 @@ if ((typeof window !== 'undefined') && window.Vue) {
 	install(window.Vue);
 }
 
+export {
+	AddressForm
+};
+
 export default {
-	install
+	install,
+	AddressForm
 };
