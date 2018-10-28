ac31238a5ef77b68753b47f9171d81535fad9f86
Saran Tanpituckpong <sarunta@gmail.com>
2018-10-28 21:12:59 +0700

    :speech_balloon: Update CDN urls

--- src/constants/cdn.ts
+++ src/constants/cdn.ts
@@ -13,17 +13,17 @@ export const jsDelivr: CDN = {
 		{
 			fileName: 'vue-thailand-address.css',
 			unmin:
-				'https://cdn.jsdelivr.net/npm/vue-thailand-address@3.0.0-rc.3/dist/vue-thailand-address.css',
+				'https://cdn.jsdelivr.net/npm/vue-thailand-address@3/dist/vue-thailand-address.css',
 			min:
-				'https://cdn.jsdelivr.net/npm/vue-thailand-address@3.0.0-rc.3/dist/vue-thailand-address.min.css'
+				'https://cdn.jsdelivr.net/npm/vue-thailand-address@3/dist/vue-thailand-address.min.css'
 		}
 	],
 	js: {
 		fileName: ['db.web.js', 'vue-thailand-address.web.js'],
 		unmin:
-			'https://cdn.jsdelivr.net/combine/npm/vue-thailand-address@3.0.0-rc.3/dist/db.web.js,npm/vue-thailand-address@3.0.0-rc.3/dist/vue-thailand-address.web.js',
+			'https://cdn.jsdelivr.net/combine/npm/vue-thailand-address@3/dist/db.web.js,npm/vue-thailand-address@3/dist/vue-thailand-address.web.js',
 		min:
-			'https://cdn.jsdelivr.net/combine/npm/vue-thailand-address@3.0.0-rc.3/dist/db.web.min.js,npm/vue-thailand-address@3.0.0-rc.3'
+			'https://cdn.jsdelivr.net/combine/npm/vue-thailand-address@3/dist/db.web.min.js,npm/vue-thailand-address@3'
 	}
 };
 
@@ -32,24 +32,24 @@ export const unpkg: CDN = {
 		{
 			fileName: 'vue-thailand-address.css',
 			unmin:
-				'https://unpkg.com/vue-thailand-address@3.0.0-rc.3/dist/vue-thailand-address.css',
+				'https://unpkg.com/vue-thailand-address@3/dist/vue-thailand-address.css',
 			min:
-				'https://unpkg.com/vue-thailand-address@3.0.0-rc.3/dist/vue-thailand-address.min.css'
+				'https://unpkg.com/vue-thailand-address@3/dist/vue-thailand-address.min.css'
 		}
 	],
 	js: [
 		{
 			fileName: 'db.web.js',
 			unmin:
-				'https://unpkg.com/vue-thailand-address@3.0.0-rc.3/dist/db.web.js',
+				'https://unpkg.com/vue-thailand-address@3/dist/db.web.js',
 			min:
-				'https://unpkg.com/vue-thailand-address@3.0.0-rc.3/dist/db.web.min.js'
+				'https://unpkg.com/vue-thailand-address@3/dist/db.web.min.js'
 		},
 		{
 			fileName: 'vue-thailand-address.web.js',
 			unmin:
-				'https://unpkg.com/vue-thailand-address@3.0.0-rc.3/dist/vue-thailand-address.web.js',
-			min: 'https://unpkg.com/vue-thailand-address@3.0.0-rc.3'
+				'https://unpkg.com/vue-thailand-address@3/dist/vue-thailand-address.web.js',
+			min: 'https://unpkg.com/vue-thailand-address@3'
 		}
 	]
 };
